af309778fb2ebebe9dc4a53b0760953de81330a7
JaydenLee <jayden.lee@hexainitiative.com>
2021-05-25 23:23:56 +0800

    [] Update the layout and logic for the screen 2

--- app/src/main/java/com/jayden/twexam/ui/Screen2Fragment.kt
+++ app/src/main/java/com/jayden/twexam/ui/Screen2Fragment.kt
@@ -1,16 +1,14 @@
 package com.jayden.twexam.ui
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.os.bundleOf
+import android.view.*
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.jayden.twexam.R
-import com.jayden.twexam.databinding.FragmentScreen1Binding
 import com.jayden.twexam.databinding.FragmentScreen2Binding
 import com.jayden.twexam.model.UserModel
 
@@ -24,6 +22,7 @@ class Screen2Fragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_screen2,
@@ -35,11 +34,38 @@ class Screen2Fragment : Fragment() {
         return binding.root
     }
 
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.screen2_option_menu, menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_cancel -> {
+                findNavController().popBackStack()
+            }
+            R.id.menu_save -> {
+                if (isFormValidated()) {
+                    //update the previous screen
+                    findNavController().popBackStack()
+                } else {
+                    val imm =
+                        requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+                    val mySnackbar = Snackbar.make(
+                        this.requireView(),
+                        "Please fill in all the form",
+                        Snackbar.LENGTH_SHORT
+                    )
+                    mySnackbar.show()
+                }
+            }
+        }
+        return true
+    }
+
+    private fun isFormValidated(): Boolean {
+        return binding.etFirstname.text.isNotEmpty() && binding.etEmail.text.isNotEmpty()
+                && binding.etLastname.text.isNotEmpty() && binding.etPhone.text.isNotEmpty()
+    }
 
 }
